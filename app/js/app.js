b9f04c6f0740ca448e832e294c33758262147644
rockwotj <rockwotj@rose-hulman.edu>
2015-04-01 21:26:48 -0400

    Finished Polymer UI
    So much polymer, but cool stuff... moving onto Endpoints!

--- app/js/app.js
+++ app/js/app.js
@@ -2,28 +2,28 @@
 
 var App = angular.module('App', ['ngRoute', 'AppControllers', 'AppServices']);
 
-App.config(function($routeProvider) {
-  $routeProvider.when('/', {
-    controller : 'MainCtrl',
-    templateUrl: '/partials/main.html',
-    resolve    : { 'guestService': 'guestService' },
-  });
-  $routeProvider.when('/invite', {
-    controller : 'InsertCtrl',
-    templateUrl: '/partials/insert.html',
-  });
-  $routeProvider.when('/update/:id', {
-    controller : 'UpdateCtrl',
-    templateUrl: '/partials/update.html',
-    resolve    : { 'guestService': 'guestService' },
-  });
-  $routeProvider.otherwise({
-    redirectTo : '/'
-  });
-});
-
-App.config(function($httpProvider) {
-  $httpProvider.interceptors.push('myHttpInterceptor');
-});
+//App.config(function($routeProvider) {
+//  $routeProvider.when('/', {
+//    controller : 'MainCtrl',
+//    templateUrl: '/partials/main.html',
+//    resolve    : { 'guestService': 'guestService' },
+//  });
+//  $routeProvider.when('/invite', {
+//    controller : 'InsertCtrl',
+//    templateUrl: '/partials/insert.html',
+//  });
+//  $routeProvider.when('/update/:id', {
+//    controller : 'UpdateCtrl',
+//    templateUrl: '/partials/update.html',
+//    resolve    : { 'guestService': 'guestService' },
+//  });
+//  $routeProvider.otherwise({
+//    redirectTo : '/'
+//  });
+//});
+//
+//App.config(function($httpProvider) {
+//  $httpProvider.interceptors.push('myHttpInterceptor');
+//});
 
 
--- model.py
+++ model.py
@@ -1,49 +1,14 @@
 from google.appengine.ext import ndb
 from endpoints_proto_datastore.ndb.model import EndpointsModel
 
-class Guest(ndb.Model):
-  first = ndb.StringProperty()
-  last = ndb.StringProperty()
-
-
-def AllGuests():
-  return Guest.query()
-
-
-def UpdateGuest(id, first, last):
-  guest = Guest(id=id, first=first, last=last)
-  guest.put()
-  return guest
-
-
-def InsertGuest(first, last):
-  guest = Guest(first=first, last=last)
-  guest.put()
-  return guest
-
-
-def DeleteGuest(id):
-  key = ndb.Key(Guest, id)
-  key.delete()
-
-class SecretWord(EndpointsModel):
-    """
-    List of these are all of the registered available games, note word is not sent via Endpoints    
-    """
-    message_field_schema = ("entityKey", "creator", "word_length", "last_touch_date_time")
-    word = ndb.StringProperty() #"basic"
-    creator = ndb.StringProperty() #"Rocky" 
-    word_length = ndb.ComputedProperty(lambda (self): len(self.word))
-    last_touch_date_time = ndb.DateTimeProperty(auto_now=True)
+SECRET_WORD_LIST = []
 
 class HangmanGame(EndpointsModel):
     """
-    Parent key of User,
-    Once a user "selects a SecretWord, their game and progress is represented by this model"    
+    Id of User
     """
     message_field_schema = ("entityKey", "creator", "display_word", "guesses", "last_touch_date_time")
-    word = ndb.KeyProperty(kind=SecretWord)
-    creator = ndb.ComputedProperty(lambda(self): self.word.get().creator)
+    word = ndb.StringProperty() #basic
     display_word = ndb.StringProperty() #"ba**c"
     guesses = ndb.StringProperty() #"abcyz"
     last_touch_date_time = ndb.DateTimeProperty(auto_now=True)
